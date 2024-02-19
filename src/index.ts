749bc6356ed6e24b535f0d188e6d5550c4dece36
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-02-19 23:21:29 +0800

    chore: 换一种实现思路直接将复制版中的内容去除结尾空格再粘贴

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,17 @@
-import { addEventListener, createRange, getCopyText, getLineText, getPosition, updateText } from '@vscode-use/utils'
-import type { Disposable, ExtensionContext } from 'vscode'
+import { getCopyText, setCopyText } from '@vscode-use/utils'
 import { trim } from 'lazy-js-utils'
 
-// bug: 修复回车的时候也会触发，将换行给取消的问题
-export async function activate(context: ExtensionContext) {
-  const disposes: Disposable[] = []
-
-  disposes.push(addEventListener('text-change', async ({ contentChanges }) => {
-    if (contentChanges.length === 1) {
-      const change = contentChanges[0]
-      const text = change.text
-      let lineText: string = ''
-      try {
-        lineText = getLineText(change.range.start.line) || ''
-      }
-      catch (error) {
-
-      }
-      if (!text.trim() || text.trim() === lineText)
-        return
-      const range = change.range
-      const copyText = await getCopyText()
-      if (copyText === text) {
-        const trimedText = trim(text, 'post')
-        if (trimedText === text)
-          return
-        const endPosition = getPosition(change.rangeOffset + text.length)
-        const textRange = createRange(range.start.line, range.start.character, endPosition.line, endPosition.character)
-        updateText((edit) => {
-          edit.replace(textRange, trimedText)
-        })
-      }
-    }
-  }))
-
-  context.subscriptions.push(...disposes)
+let timer: any = null
+export async function activate() {
+  timer = setInterval(async () => {
+    const copyText = await getCopyText()
+    const trimEndCopyText = trim(copyText, 'post')
+    if (trimEndCopyText === copyText)
+      return
+    setCopyText(trimEndCopyText)
+  }, 60)
 }
 
 export function deactivate() {
-
+  clearInterval(timer)
 }
