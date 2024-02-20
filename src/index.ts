514759b6d57ba5131ac00b904604ae672c47403e
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-02-20 10:22:41 +0800

    fix: 直接复制当前行粘贴的情况下换行被移除的问题

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { getCopyText, setCopyText } from '@vscode-use/utils'
+import { getCopyText, getLineText, getSelection, setCopyText } from '@vscode-use/utils'
 import { trim } from 'lazy-js-utils'
 
 let timer: any = null
@@ -6,6 +6,12 @@ export async function activate() {
   timer = setInterval(async () => {
     const copyText = await getCopyText()
     const trimEndCopyText = trim(copyText, 'post')
+    const selection = getSelection()
+    if (selection) {
+      const lineText = getLineText(selection.line)
+      if (lineText === trimEndCopyText)
+        return
+    }
     if (trimEndCopyText === copyText)
       return
     setCopyText(trimEndCopyText)
